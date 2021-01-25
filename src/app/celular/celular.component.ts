5e5f9f0d235e370ee5452605688a4c7be47fd595
Camilohl10 <camilo.hdez.lara@gmail.com>
2021-01-25 11:30:46 -0600

    Se agregó codigo html a traves de un enlace de url, también se agregó estilo dentro de componente celular, el componente celular usa el componente videjuego, los dos se usan dentro de app.component.ts

--- src/app/celular/celular.component.ts
+++ src/app/celular/celular.component.ts
@@ -4,16 +4,22 @@ import {Component,ViewEncapsulation} from '@angular/core';
 @Component({
     selector: 'celular',
     template: `
-    <div class="card">
-    <h1 class="titulo">Hola hola</h1>
+    <div class="card">    
+    <h1 class="titulo">Componente de celular</h1>
+    <!-- Usando componente videojuego dentro del componente celular -->
     <videojuego></videojuego>
     </div>
+
     <style>
       .titulo{
+        color: blue;
         text-align: center;
+        border: 1px solid green;
+        margin:0;
+
       }
       .card {        
-        
+        border: 1px solid orange;
         background-size: cover;
         box-shadow: 0 10px 20px rgba(0,0,0,0.19), 
                     0 6px 6px rgba(0,0,0,0.23);
--- src/app/videojuego/videojuego.component.ts
+++ src/app/videojuego/videojuego.component.ts
@@ -2,20 +2,15 @@ import {Component,ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector: 'videojuego',
-    template: `    
-    <h2>Componente de videojuego</h2>
-    <ul>
-        <li>GTA</li>
-        <li>Prince of persia</li>
-        <li>Tekken</li>
-        <li>Mario</li>
-        <li>Zeta</li>
-    </ul>
-    `,    
+    templateUrl:'./videojuego.component.html'  
 })
 
 export class VideojuegoComponent {
+    public titulo:string;
+    public listado:string;
     constructor(){
+        this.titulo="Componente de videjuegos";
+        this.listado="Listado de los juegos más populares."
         console.log("Se ha cargado el componente: videojuego.component.ts");
     }
 }
