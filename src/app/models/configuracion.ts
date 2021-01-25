d7c765c2328ee6121d01672b17503f8c346ed2ef
Camilohl10 <camilo.hdez.lara@gmail.com>
2021-01-25 13:29:21 -0600

    Se agrega carpeta models

---
+++ src/app/models/configuracion.ts
@@ -0,0 +1,6 @@
+export var Configuracion ={
+    color: "red",
+    fondo: "#eee",
+    titulo: "Al máster en JavaScript y Angular!",
+    descripcion: "Aprendiendo Angular con Víctor Robles el curso master-estudiante Camilo"
+}
