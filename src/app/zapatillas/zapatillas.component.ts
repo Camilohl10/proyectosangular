7f8f22356314c59937f17f0648146c2bf4132da4
Camilohl10 <camilo.hdez.lara@gmail.com>
2021-01-25 13:45:46 -0600

    Se implementa modelos en angular

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -1,11 +1,24 @@
 import { from } from "rxjs";
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+/* Se importa la clase Zapatilla de modelo/zapatilla*/
+import { Zapatilla } from '../models/zapatilla'
 
 @Component({
     selector: 'zapatilla',
     templateUrl: './zapatillas.component.html'
 })
 
-export class ZapatillasComponent{
+export class ZapatillasComponent implements OnInit{
     public titulo: string = "Componente de Zapatillas";
+    /* Se crea una variable de clase de tipo Array con tipo de datos Clase Zapatilla */
+    public zapatillas:Array<Zapatilla>;
+
+    constructor(){
+        this.zapatillas=[
+            new Zapatilla('Reebook Classic', 'Reebook','Blanco',80,true)
+        ];
+    }
+    ngOnInit(){
+        console.log(this.zapatillas);
+    }
 }
