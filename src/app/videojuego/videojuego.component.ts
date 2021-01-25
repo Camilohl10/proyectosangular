8de14c15ba7b19f92d4971ac9f01e72192da41e5
Camilohl10 <camilo.hdez.lara@gmail.com>
2021-01-25 03:06:33 -0600

    Se creo carpeta de componente videojuegos

---
+++ src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'videojuego',
+    template: `
+    <h2>Componente de videojuego</h2>
+    <ul>
+        <li>GTA</li>
+        <li>Prince of persia</li>
+        <li>Tekken</li>
+        <li>Mario</li>
+    </ul>
+    `,
+})
+export class VideojuegoComponent {}
