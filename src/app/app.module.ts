94f4c39fe92cd4619f4e78f9b20a6e94c51ed8f6
Camilohl10 <camilo.hdez.lara@gmail.com>
2021-01-25 11:57:09 -0600

    Se creo el componente cursos con scaffolt ng g component nombrecomp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 /* Importar componente creado */
 import { VideojuegoComponent} from './videojuego/videojuego.component';
 import { CelularComponent} from './celular/celular.component';
+import { ZapatillasComponent} from './zapatillas/zapatillas.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideojuegoComponent,
     CelularComponent,
+    ZapatillasComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule
